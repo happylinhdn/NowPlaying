da470b627d0c422601d712d38eab09884116fc8a
Linh Do <linh.do@easybook.com>
2019-03-10 01:16:00 +0700

    Add Constant and View factory

---
+++ app/src/main/java/happy/linhdn/nowplaying/util/Constant.java
@@ -0,0 +1,6 @@
+package happy.linhdn.nowplaying.util;
+
+public class Constant {
+    public static final String IMG_API_URL = "https://image.tmdb.org/t/p/w500/";
+    public static final String API_KEY = "cf929ad6853be9724fcd6550057820a7";
+}
